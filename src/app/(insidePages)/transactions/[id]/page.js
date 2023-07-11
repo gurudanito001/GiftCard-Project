033056036cbf0fc254dd4ce2f9b624a3b5b81ab5
Daniel Nwokocha <gurudanito001@gmail.com>
2023-07-11 09:17:08 +0100

    removed the apostrophes

--- src/app/(insidePages)/transactions/[id]/page.js
+++ src/app/(insidePages)/transactions/[id]/page.js
@@ -73,7 +73,7 @@ const TransactionDetails = () =>{
                             </button>
                         </h2>
                         <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                            <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the first item's accordion body.</div>
+                            <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the first items accordion body.</div>
                         </div>
                     </div>
                 </div>
