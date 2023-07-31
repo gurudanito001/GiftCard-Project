6704bdbf82fff1578a9ec31e0720e977c6a7a058
Daniel Nwokocha <gurudanito001@gmail.com>
2023-07-31 07:04:59 +0100

    Finished the herosection of landing page

--- src/components/publicLayout.js
+++ src/components/publicLayout.js
@@ -4,8 +4,12 @@ const PublicLayout = ({ children }) => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg accent-bg sticky-top">
-        <div className="container">
+
+      <nav className="navbar navbar-expand-lg accent-bg sticky-top d-flex flex-column">
+        <div className="border-bottom d-flex w-100">
+          <a className="ms-auto small pe-3 text-white text-decoration-none" href="https://wa.me/2348186736264" target="_blank"><i className="fa-brands fa-whatsapp fs-6"></i> +234 818 673 6264</a>
+        </div>
+        <div className="container py-2">
           <a className="navbar-brand text-decoration-none primary-text fw-bold fs-3" href="/">
             Peniga
           </a>
@@ -20,58 +24,64 @@ const PublicLayout = ({ children }) => {
           </button>
           <ul className="d-none d-lg-flex navbar-nav gap-2 flex-fill justify-content-center">
             <li className="nav-item">
-              <a className="nav-link nav-link-lg primary-text" aria-current="page" href="/#home">Home</a>
+              <a className="nav-link primary-text" aria-current="page" href="/#home">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link nav-link-lg primary-text" aria-current="page" href="/#ourServices">Our Services</a>
+              <a className="nav-link primary-text" aria-current="page" href="/#ourServices">Our Services</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link nav-link-lg primary-text" aria-current="page" href="/#whyChooseUs">Why Choose Us</a>
+              <a className="nav-link primary-text" aria-current="page" href="/#whyChooseUs">Why Choose Us</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link nav-link-lg primary-text" aria-current="page" href="/marketplace">Market Place</a>
+              <a className="nav-link primary-text" aria-current="page" href="/marketplace">Market Place</a>
             </li>
           </ul>
           <ul className="d-none d-lg-flex navbar-nav gap-2">
             <li>
-              <a type="button" className="btn login-btn outline-primary-btn" href="/auth/login">Log In</a>
+              <a type="button" className="btn outline-primary-btn px-4 py-2 fw-bold" href="#">Log In</a>
             </li>
             <li>
-              <a className="btn accent-btn sign-up-btn"
-                type="button " href="/auth/register">Register
+              <a className="btn accent-btn sign-up-btn px-4 py-2 fw-bold"
+                type="button " href="#">Register
               </a>
             </li>
           </ul>
         </div>
       </nav>
+
+
+
+
       <aside className="offcanvas offcanvas-end accent-bg mb-auto" data-bs-scroll="true" id="navmenu">
         <div className="offcanvas-header pe-5 pt-4 justify-content-end mt-1 px-0">
           <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div className="offcanvas-body align-items-center px-4 d-flex flex-column gap-4">
           <ul className="navbar-nav gap-3">
-            <li className="nav-item">
-              <a className="nav-link py-0 primary-text" aria-current="page" href="./index.html#home">Home</a>
+             <li className="nav-item">
+              <a className="nav-link primary-text" aria-current="page" href="/#home">Home</a>
             </li>
             <li className="nav-item">
-              <a className="py-0 nav-link primary-text" aria-current="page" href="./index.html#ourServices">Our Services</a>
+              <a className="nav-link primary-text" aria-current="page" href="/#ourServices">Our Services</a>
             </li>
             <li className="nav-item">
-              <a className="py-0 nav-link primary-text" aria-current="page" href="./index.html#whyChooseUs">Why Choose Us</a>
+              <a className="nav-link primary-text" aria-current="page" href="/#whyChooseUs">Why Choose Us</a>
             </li>
-
+            <li className="nav-item">
+              <a className="nav-link primary-text" aria-current="page" href="/marketplace">Market Place</a>
+            </li>
+          </ul>
 
             <ul className="navbar-nav gap-2">
               <li>
-                <a type="button" className="btn outline-primary-btn" href="./login.html">Log In</a>
+                <a type="button" className="btn outline-primary-btn px-4 py-2 fw-bold" href="/auth/login">Log In</a>
               </li>
               <li>
-                <a className="btn accent-btn"
-                  type="button " href="./signUp.html">Register
+                <a className="btn accent-btn sign-up-btn px-4 py-2 fw-bold"
+                  type="button " href="/auth/register">Register
                 </a>
               </li>
             </ul>
-          </ul>
         </div>
       </aside>
       <main style={{ height: "100vh" }} className='d-flex flex-column'>
--- src/views/landing/heroSection.js
+++ src/views/landing/heroSection.js
@@ -3,19 +3,36 @@ import Image from "next/image";
 const HeroSection = () => {
 
   return (
-    <section id="home" className="hero-section" style={{background: "#531eba !important"}}>
-      <div className="container d-flex flex-column align-items-center gap-4">
-        <div className="hero-content text-white align-items-center text-center d-flex flex-column gap-2">
-          <h1 className="mb-0 col-lg-9 heading">Trade Giftcards at best rates in Nigeria.</h1>
-          <p className="mb-0 col-lg-7 fs-5">Join the leading peer-to-peer platform to
-            buy and sell Giftcard with 10 million
-            people just like you.
-          </p>
+    <>
+      <section id="home" className="hero-section" style={{ background: "#531eba !important" }}>
+        <div className="container">
+          <div className="d-flex flex-column flex-lg-row text-white text-start gap-2">
+            <section className="col-12 col-lg pe-lg-5 d-flex flex-column py-5">
+              <h1 className="mb-0 heading mb-4 mt-auto">Trade Giftcards at best rates in Nigeria.</h1>
+              <p className="fs-6 fs-lg-4">Join the leading peer-to-peer platform to
+                buy and sell Giftcard from merchants and sellers
+              </p>
+              <div className="mt-5 mb-auto d-flex">
+                <a type="button" className="btn outline-primary-btn px-5 py-3 fw-bold" href="#">Get Started</a>
+                <a type="button" className="btn btn-success px-5 py-3 fw-bold ms-3" href="https://wa.me/2348186736264" target="_blank"><i className="fa-brands fa-whatsapp fs-6"></i> Chat</a>
+              </div>
+              
+            </section>
+            <section className="col-12 col-lg py-lg-5">
+              <Image width={100} height={100} alt="Monitor showing charts and a human beside the monitor" className="hero-img img img-fluid" src="/images/undraw_data_trends_re_2cdy.svg" />
+            </section>
+          </div>
         </div>
 
-        <Image width={100} height={100} alt="Monitor showing charts and a human beside the monitor" className="hero-img img-fluid" src="/images/undraw_data_trends_re_2cdy.svg" />
+      </section>
+      <div className="d-none d-lg-block">
+        <svg style={{ width: "100%", height: "300px" }} width="500" height="80" viewBox="0 0 500 80" preserveAspectRatio="none">
+          <path d="M0,0 L0,40 Q250,80 500,40 L500,0 Z" fill="#531eba" />
+        </svg>
       </div>
-    </section>
+
+    </>
+
   )
 }
 
